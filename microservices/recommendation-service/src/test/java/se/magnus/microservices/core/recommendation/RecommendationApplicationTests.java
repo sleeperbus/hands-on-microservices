cf5a6056bc160c7268358f2b42b5147058203241
지호준 <hj@tossinvest.com>
2023-01-08 15:35:47 +0900

    * recommendation service api 테스트

--- microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/RecommendationApplicationTests.java
+++ microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/RecommendationApplicationTests.java
@@ -1,13 +1,84 @@
 package se.magnus.microservices.core.recommendation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RecommendationApplicationTests {
+    @Autowired
+    private WebTestClient client;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void getRecommendationsByProductId() {
+        int productId = 1;
 
+        client.get()
+                .uri("/recommendation?productId=" + productId)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.length()").isEqualTo(3)
+                .jsonPath("$[0].productId").isEqualTo(productId);
+    }
+
+    @Test
+    public void getRecommendationsMissingParameter() {
+        client.get()
+                .uri("/recommendation")
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(BAD_REQUEST)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/recommendation")
+                .jsonPath("$.message").isEqualTo("Required query parameter 'productId' is not present.");
+    }
+
+    @Test
+    public void getRecommendationsInvalidParameter() {
+        client.get()
+                .uri("/recommendation?productId=no-integer")
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(BAD_REQUEST)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/recommendation")
+                .jsonPath("$.message").isEqualTo("Type mismatch.");
+    }
+
+    @Test
+    public void getRecommendationsNotFound() {
+        int productIdNotFound = 113;
+        client.get()
+                .uri("/recommendation?productId=" + productIdNotFound)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.length()").isEqualTo(0);
+    }
+
+    @Test
+    public void getRecommendationInvalidParameterNegativeValue() {
+        int productNegativeId = -1;
+        client.get()
+                .uri("/recommendation?productId=" + productNegativeId)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/recommendation")
+                .jsonPath("$.message").isEqualTo("Invalid productId: " + productNegativeId);
+    }
 }
