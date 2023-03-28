3a0653023ed785ee148c7af3426d18e710188c66
Hojoon Ji <sleeperbus@gmail.com>
2023-03-29 08:34:50 +0900

    * RecommendationEntity test

---
+++ microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/PersistenceTests.java
@@ -0,0 +1,106 @@
+package se.magnus.microservices.core.recommendation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import se.magnus.microservices.core.recommendation.persistent.RecommendationEntity;
+import se.magnus.microservices.core.recommendation.persistent.RecommendationRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+class PersistenceTests {
+    @Autowired
+    private RecommendationRepository repository;
+    private RecommendationEntity savedEntity;
+
+    @BeforeEach
+    void setupDb() {
+        repository.deleteAll();
+        RecommendationEntity entity = new RecommendationEntity(1, 1, "a", 3, "new book");
+        savedEntity = repository.save(entity);
+
+        assertEqualsRecommendation(entity, savedEntity);
+    }
+
+    private void assertEqualsRecommendation(RecommendationEntity expected, RecommendationEntity actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getVersion(), actual.getVersion());
+        assertEquals(expected.getProductId(), actual.getProductId());
+        assertEquals(expected.getRecommendationId(), actual.getRecommendationId());
+        assertEquals(expected.getAuthor(), actual.getAuthor());
+        assertEquals(expected.getRating(), actual.getRating());
+        assertEquals(expected.getContent(), actual.getContent());
+    }
+
+    @Test
+    public void create() {
+        RecommendationEntity newEntity = new RecommendationEntity(1, 2, "a", 3, "new book");
+        repository.save(newEntity);
+
+        RecommendationEntity foundEntity = repository.findById(newEntity.getId()).get();
+        assertEqualsRecommendation(newEntity, foundEntity);
+        assertEquals(2, repository.count());
+    }
+
+    @Test
+    public void update() {
+        savedEntity.setAuthor("b");
+        repository.save(savedEntity);
+
+        RecommendationEntity foundEntity = repository.findById(savedEntity.getId()).get();
+        assertEquals(1, foundEntity.getVersion());
+        assertEquals("b", foundEntity.getAuthor());
+    }
+
+    @Test
+    public void delete() {
+        repository.deleteAll();
+        assertFalse(repository.existsById(savedEntity.getId()));
+    }
+
+    @Test
+    public void getByProductId() {
+        List<RecommendationEntity> entityList = repository.findByProductId(savedEntity.getProductId());
+        assertEquals(1, entityList.size());
+        assertEqualsRecommendation(savedEntity, entityList.get(0));
+    }
+
+    @Test
+    public void duplicateError() {
+        assertThrows(DuplicateKeyException.class, () -> {
+            RecommendationEntity entity = new RecommendationEntity(1, 1, "a", 3, "new book");
+            repository.save(entity);
+        });
+    }
+
+    @Test
+    public void optimisticLocking() {
+        RecommendationEntity entity1 = repository.findById(savedEntity.getId()).get();
+        RecommendationEntity entity2 = repository.findById(savedEntity.getId()).get();
+
+        entity1.setAuthor("b");
+        repository.save(entity1);
+        try {
+            entity2.setAuthor("c");
+            repository.save(entity2);
+            fail("Expected an OptimisticLockingFailureException");
+
+        } catch (OptimisticLockingFailureException e) {}
+
+        RecommendationEntity updatedEntity = repository.findById(savedEntity.getId()).get();
+        assertEquals(1, updatedEntity.getVersion());
+        assertEquals("b", updatedEntity.getAuthor());
+    }
+
+}
