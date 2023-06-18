c2e30cfb1dc6237370b905ffd4f75745b9c92ac7
Hojoon Ji <sleeperbus@gmail.com>
2023-06-18 17:24:04 +0900

    * docker로 올릴 때 gateway 설정에서 active profile 지정이 안되어 있었다.

--- spring-cloud/gateway/src/test/java/se/magnus/springcloud/gateway/GatewayApplicationTests.java
+++ spring-cloud/gateway/src/test/java/se/magnus/springcloud/gateway/GatewayApplicationTests.java
@@ -3,7 +3,7 @@ package se.magnus.springcloud.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"eureka.client.enabled=false"})
 class GatewayApplicationTests {
 
 	@Test
