e12972fe84119ebdf6a8e0173654dcf8dc59a2ca
Hojoon Ji <sleeperbus@gmail.com>
2023-06-18 21:06:36 +0900

    * health check 할 때 각 서버를 80포트로 찾고 있어서 강제로 8080으로 맞춰주었다. 예제코드에서는 그냥 application name만 명시했었는데...

--- spring-cloud/gateway/src/main/java/se/magnus/springcloud/gateway/HealthCheckConfiguration.java
+++ spring-cloud/gateway/src/main/java/se/magnus/springcloud/gateway/HealthCheckConfiguration.java
@@ -31,17 +31,17 @@ public class HealthCheckConfiguration {
     ReactiveHealthContributor healthCheckContributor() {
         final Map<String, ReactiveHealthIndicator> registry = new LinkedHashMap<>();
 
-        registry.put("product", () -> getHealth("http://product"));
-        registry.put("recommendation", () -> getHealth("http://recommendation"));
-        registry.put("review", () -> getHealth("http://review"));
-        registry.put("product-composite", () -> getHealth("http://product-composite"));
+        registry.put("product", () -> getHealth("http://product:8080"));
+        registry.put("recommendation", () -> getHealth("http://recommendation:8080"));
+        registry.put("review", () -> getHealth("http://review:8080"));
+        registry.put("product-composite", () -> getHealth("http://product-composite:8080"));
 
         return CompositeReactiveHealthContributor.fromMap(registry);
     }
 
     private Mono<Health> getHealth(String baseUrl) {
         String url = baseUrl + "/actuator/health";
-        LOG.debug("Setting up a call to the Health API on URL: {}", url);
+        LOG.info("Setting up a call to the Health API on URL: {}", url);
         return webClient.get().uri(url).retrieve().bodyToMono(String.class)
                 .map(s -> new Health.Builder().up().build())
                 .onErrorResume(ex -> Mono.just(new Health.Builder().down(ex).build()))
