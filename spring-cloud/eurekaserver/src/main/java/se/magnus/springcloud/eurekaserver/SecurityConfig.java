d48bdf178da5ff93bf648bf6990c7f7a6c6ed417
Hojoon Ji <sleeperbus@gmail.com>
2023-06-25 22:03:49 +0900

    * 잘 안된다. 포기해야 할까.

---
+++ spring-cloud/eurekaserver/src/main/java/se/magnus/springcloud/eurekaserver/SecurityConfig.java
@@ -0,0 +1,44 @@
+package se.magnus.springcloud.eurekaserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final String username;
+    private final String password;
+
+    public SecurityConfig(
+            @Value("${app.eureka-username}") String username,
+            @Value("${app.eureka-password}") String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(username).password(password)
+                .authorities("USER");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
